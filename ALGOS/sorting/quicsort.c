7e3d12ea4df1c9b73b4c38138aaf8d38b6bfe476
sargun2004 <132887555+sargun2004@users.noreply.github.com>
2023-07-15 10:47:31 +0530

    Add files via upload

---
+++ ALGOS/sorting/quicsort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1) {
+        do {
+            i++;
+        } while (arr[i] < pivot);
+
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j) {
+            return j;
+        }
+
+        swap(&arr[i], &arr[j]);
+    }
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot_index = partition(arr, low, high);
+        quicksort(arr, low, pivot_index);
+        quicksort(arr, pivot_index + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    quicksort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
